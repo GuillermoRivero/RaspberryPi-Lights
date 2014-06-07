68effc35c020699d0f7b6a25e36d12a782c413df
Guillermo Rivero <alu0100702149.ull.edu.es>
2014-06-07 20:49:46 +0100

    Añadido PiPipper

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,24 +1,5 @@
 $(document).ready(function() {
 
-	var gpio = require("pi-gpio");
-
-	gpio.open(18, "output", function(err) {
-		console.log(err);	
-	});
-
-	$('#encender').click(function() {
-
-		gpio.write(18, 1, function() {}); /*Pin 18 Encendido*/
-   
-	});  
-
-	$('#apagar').click(function() {
-		gpio.write(18, 0, function() {}); /*Pin 16 Apagado*/
-	});
-
-
-
-
 });
 
 
